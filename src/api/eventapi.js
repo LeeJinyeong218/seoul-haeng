8bc33ceae17eb06fb44116826fd9cbd990a0f398
LeeJinyoung218 <amy021899@gmail.com>
2023-05-29 14:10:05 +0900

    add cors

--- src/api/eventapi.js
+++ src/api/eventapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 async function getEvents({start, end}) {
     return axios.get(
-        `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_API_KEY}/json/culturalEventInfo/${start.toString()}/${end.toString()}/`
+        `/api/${process.env.REACT_APP_API_KEY}/json/culturalEventInfo/${start.toString()}/${end.toString()}/`
     ).catch((error) => {
         return console.log("error alert! \n"+error);
     }).then((response) => {
@@ -11,7 +11,7 @@ async function getEvents({start, end}) {
 
 async function getEventsNum() {
     const response = await axios.get(
-        `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_API_KEY}/json/culturalEventInfo/1/1/`
+        `/api/${process.env.REACT_APP_API_KEY}/json/culturalEventInfo/1/1/`
     );
     console.log(response);
     return response.data['culturalEventInfo']['list_total_count'];
